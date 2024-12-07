a12fd12b6c5c599f9965c48815c5f9a7c88fcb76
Fiyin Osifala <123388270+Fiyinosif@users.noreply.github.com>
2024-12-06 21:15:57 -0600

    Update Product_ElectronicDevice.py

--- Product_ElectronicDevice.py
+++ Product_ElectronicDevice.py
@@ -1,9 +1,10 @@
-# Version: v0.1
-# Date Last Updated: 11-30-2024
-# %% STANDARDS - DO NOT include this block in a new module
+#Version: v0.1
+#Date Last Updated: 11-30-2024
+#%% STANDARDS - DO NOT include this block in a new module
 
-# %% MODULE BEGINS
-module_name = 'second_Parent_child_classes'
+
+#%% MODULE BEGINS
+module_name = 'Product_Electronic_classes'
 '''
 Version: v0.1
 Description:
@@ -19,27 +20,26 @@ Notes:
 The module follows the provided standard template.
 '''
 
-# %% IMPORTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% IMPORTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from copy import deepcopy as dpcpy
 import datetime as dt
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
 
-# %% USER INTERFACE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% USER INTERFACE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # No user interface components for this module.
 
-# %% CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 DATE_FORMAT = "%Y-%m-%d"  # Define date format constant
 FILE_PATH = "product_file.csv"  # Define the file to be read
 
-# %% CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Configuration settings are implemented as constants for reusability.
 
-# %% INITIALIZATIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% INITIALIZATIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Initialize reusable variables or data structures if needed.
 
-# %% DECLARATIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% DECLARATIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Global declarations Start Here
 # Class definitions Start Here
 
@@ -87,63 +87,24 @@ class Product:
         '''
         return f"{self.name} - ${self.price:.2f}, Manufactured: {self.manufactured_date}"
 
-    @staticmethod
-    def plot_price_distribution(products):
-        '''
-        Plot distribution of prices for a list of products.
-        '''
-        prices = [product.price for product in products]
-        plt.hist(prices, bins=10, color='blue', edgecolor='black')
-        plt.title('Price Distribution')
-        plt.xlabel('Price')
-        plt.ylabel('Frequency')
-        plt.show()
-
 class ElectronicDevice(Product):
     '''
     Child class inheriting from Product, representing electronic devices.
     '''
 
-    def __init__(self, name, price, manufactured_date, warranty_period):
+    def __init__(self, name, price, manufactured_date):
         '''
         Initialize an ElectronicDevice object.
         '''
         super().__init__(name, price, manufactured_date)
-        self.warranty_period = int(warranty_period)  # Warranty in months
+        #
 
-    def to_dict(self):
-        '''
-        Convert ElectronicDevice object attributes to a dictionary.
-        '''
-        base_dict = super().to_dict()
-        base_dict["Warranty Period (months)"] = self.warranty_period
-        return base_dict
-
-    def __str__(self):
-        '''
-        String representation of an ElectronicDevice object.
-        '''
-        return f"{super().__str__()}, Warranty: {self.warranty_period} months"
-
-    @staticmethod
-    def plot_warranty_distribution(devices):
-        '''
-        Plot distribution of warranty periods for a list of electronic devices.
-        '''
-        warranties = [device.warranty_period for device in devices]
-        plt.hist(warranties, bins=10, color='green', edgecolor='black')
-        plt.title('Warranty Period Distribution')
-        plt.xlabel('Warranty Period (months)')
-        plt.ylabel('Frequency')
-        plt.show()
-
-# %% MAIN CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#%% MAIN CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Main code starts here
 # Testing is contained in the main function.
 
-# %% SELF-RUN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+#%% SELF-RUN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 if __name__ == "__main__":
     print(f"\"{module_name}\" module begins.")
-
 
 
